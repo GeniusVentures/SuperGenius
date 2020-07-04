2ff5b99554c09f2440630443e7e76933ad34f792
kgcdream2019 <kgc.dream2019@gmail.com>
2020-07-04 17:52:38 +0800

    external project include path not find error fix

---
+++ node/node.cpp
@@ -0,0 +1,2 @@
+#include "node.hpp"
+#include "node.hpp"
---
+++ node/node.hpp
@@ -0,0 +1,9 @@
+#ifndef SUPERGENIUS_NODE_HPP
+#define SUPERGENIUS_NODE_HPP
+#include <secure/ledger.hpp>
+#include <secure/utility.hpp>
+class node {
+	sgns::keypair node_id;
+};
+
+#endif
