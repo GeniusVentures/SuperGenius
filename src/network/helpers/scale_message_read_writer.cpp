cac7b23bf6d22b4775bb59eb90a388c23a0a2aed
kgcdream2019 <kgc.dream2019@gmail.com>
2020-07-15 16:42:13 +0800

    Added network/helpers

---
+++ src/network/helpers/scale_message_read_writer.cpp
@@ -0,0 +1,14 @@
+
+
+#include "network/helpers/scale_message_read_writer.hpp"
+
+namespace sgns::network {
+  ScaleMessageReadWriter::ScaleMessageReadWriter(
+      std::shared_ptr<libp2p::basic::MessageReadWriter> read_writer)
+      : read_writer_{std::move(read_writer)} {}
+
+  ScaleMessageReadWriter::ScaleMessageReadWriter(
+      const std::shared_ptr<libp2p::basic::ReadWriter> &read_writer)
+      : read_writer_{std::make_shared<libp2p::basic::MessageReadWriterUvarint>(
+          read_writer)} {}
+}  // namespace sgns::network
