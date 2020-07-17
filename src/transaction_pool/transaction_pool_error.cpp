fa2f609722234be895f2035818faa22dca14d7eb
kgcdream2019 <kgc.dream2019@gmail.com>
2020-07-17 10:51:18 +0800

    Added transaction_pool_error lib

---
+++ src/transaction_pool/transaction_pool_error.cpp
@@ -0,0 +1,15 @@
+
+
+#include "transaction_pool/transaction_pool_error.hpp"
+
+OUTCOME_CPP_DEFINE_CATEGORY_3(sgns::transaction_pool, TransactionPoolError, e) {
+  using E = sgns::transaction_pool::TransactionPoolError;
+  switch (e) {
+    case E::TX_ALREADY_IMPORTED:
+      return "Transaction has already been imported to the pool";
+    case E::TX_NOT_FOUND:
+      return "Transaction not found in the pool";
+    case E::POOL_IS_FULL:
+      return "Transaction pool is full";
+  }
+}
