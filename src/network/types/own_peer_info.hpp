472a0a6757bfd4ee6fc8b9d0379498c8b663dbc9
kgcdream2019 <kgc.dream2019@gmail.com>
2020-07-23 23:08:58 +0800

    Added network/sync_protocol_observer

--- src/network/types/own_peer_info.hpp
+++ src/network/types/own_peer_info.hpp
@@ -10,8 +10,8 @@ namespace sgns::network {
   struct OwnPeerInfo : public libp2p::peer::PeerInfo {
     OwnPeerInfo(libp2p::peer::PeerId peer_id,
                 std::vector<libp2p::multi::Multiaddress> addresses)
-        : PeerInfo{.id = std::move(peer_id),
-                   .addresses = std::move(addresses)} {}
+        : PeerInfo{/*.id =*/ std::move(peer_id),
+                   /*.addresses =*/ std::move(addresses)} {}
   };
 
 }  // namespace sgns::network
