f69936bb8f55e32b567e5d80071f4476e78eb1bf
ruymaster <mecury1984@outlook.com>
2020-08-31 16:37:11 +0800

    update loading blockstorage

--- src/blockchain/impl/key_value_block_storage.cpp
+++ src/blockchain/impl/key_value_block_storage.cpp
@@ -53,7 +53,7 @@ namespace sgns::blockchain {
         block_storage->getLastFinalizedBlockHash();
 
     if (last_finalized_block_hash_res.has_value()) {
-      return loadExisting(storage, hasher, on_finalized_block_found, *last_finalized_block_hash_res);
+      return loadExisting(storage, hasher, on_finalized_block_found, primitives::BlockId{last_finalized_block_hash_res.value()});
     }
 
     if (last_finalized_block_hash_res
@@ -69,7 +69,7 @@ namespace sgns::blockchain {
   KeyValueBlockStorage::loadExisting(
       const std::shared_ptr<storage::BufferStorage> &storage,
       std::shared_ptr<crypto::Hasher> hasher,
-      const BlockHandler &on_finalized_block_found, const crypto::Hasher &last_finalized_block_hash) {
+      const BlockHandler &on_finalized_block_found, const primitives::BlockId &last_finalized_block_id) {
     auto block_storage = std::make_shared<KeyValueBlockStorage>(
         KeyValueBlockStorage(storage, std::move(hasher)));
 
@@ -77,7 +77,7 @@ namespace sgns::blockchain {
     //             block_storage->getLastFinalizedBlockHash());
 
     OUTCOME_TRY(block_header,
-                block_storage->getBlockHeader(last_finalized_block_hash));
+                block_storage->getBlockHeader(last_finalized_block_id));
 
     primitives::Block finalized_block;
     finalized_block.header = block_header;
