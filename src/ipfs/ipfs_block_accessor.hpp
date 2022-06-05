3dcaa9d6240c9728f84196aed4f548cdf16580a0
creativeid00 <creativeid00@gmail.com>
2022-06-06 02:02:38 +0700

    Documentation added. https://gitlab.geniusventures.io:8486/GeniusVentures/supergenius/-/issues/12

--- src/ipfs/ipfs_block_accessor.hpp
+++ src/ipfs/ipfs_block_accessor.hpp
@@ -1,3 +1,7 @@
+/**
+* Header file for IPFS block accessor
+* @author creativeid00
+*/
 #ifndef SUPERGENIUS_IPFS_BLOCK_ACCESSOR_HPP
 #define SUPERGENIUS_IPFS_BLOCK_ACCESSOR_HPP
 
@@ -6,12 +10,28 @@
 
 namespace sgns::ipfs
 {
+/** IPFS block accessor implementation
+* Block accessor allows to receive IPFS block data using DHT to discover the block providers
+*/
 class BlockAccessor
 {
 public:
+    /** Constructs an accessor object
+    * @param dht - IPFS DHT instance
+    * @param bitswap - bitswap protocol wrapper instance
+    */
     BlockAccessor(std::shared_ptr<sgns::ipfs::IpfsDHT> dht, std::shared_ptr<sgns::ipfs_bitswap::Bitswap> bitswap);
 
+    /** Sets a timeout for block request processing
+    * @param blockRequestTimeout - block request timeout
+    * Once the timeout is exceeded no block request re-submitted.
+    */
     void SetBlockRequestTimeout(std::chrono::system_clock::duration blockRequestTimeout);
+
+    /** Requests IPFS block using DHT to discover block providers
+    * @param cid - block CID
+    * @param cb - block data receivig callback
+    */
     void RequestBlock(const sgns::ipfs_bitswap::CID& cid, std::function<void(std::optional<std::string>)> cb);
 
 private:
--- src/ipfs/ipfs_dht.hpp
+++ src/ipfs/ipfs_dht.hpp
@@ -1,3 +1,7 @@
+/**
+* Header file for IPFS DHT implementation
+* @author creativeid00
+*/
 #ifndef SUPERGENIUS_IPFS_DHT_HPP
 #define SUPERGENIUS_IPFS_DHT_HPP
 
@@ -8,19 +12,39 @@
 
 namespace sgns::ipfs
 {
+/** IPFS DHT implementation
+* IPFS DHT implementation based on libp2p kademlia
+*/
 class IpfsDHT
 {
 public:
+    /** Constructs IPFS DHT object
+    * @param kademlia - libp2p kademlia instance
+    * @param bootstrapAddresses - a list of multi-adressed of IPFS bootstrap nodes
+    */
     IpfsDHT(
         std::shared_ptr<libp2p::protocol::kademlia::Kademlia> kademlia,
         std::vector<std::string> bootstrapAddresses);
 
+    /** Starts bootstraping
+    */
     void Start();
 
+    /** Asynchronous providers search
+    * @param cid - block cid that providers need to be found for
+    * @param onProvidersFound - asynchronous callback which is called once block providers found
+    * @note Each call of the method can return different set of block providers, including an empty set.
+    * Once an empty set received it doesn't mean that providers do not exist and the next cal can return a
+    * non-empty set of providers.
+    */
     void FindProviders(
         const libp2p::multi::ContentIdentifier& cid,
         std::function<void(libp2p::outcome::result<std::vector<libp2p::peer::PeerInfo>> onProvidersFound)> onProvidersFound);
 
+    /** Gets peers info
+    * @param peerId - peer identifier
+    * @param onPeerFound - callback which is called once peer info found
+    */
     void FindPeer(
         const libp2p::peer::PeerId& peerId,
         std::function<void(libp2p::outcome::result<libp2p::peer::PeerInfo>)> onPeerFound);
