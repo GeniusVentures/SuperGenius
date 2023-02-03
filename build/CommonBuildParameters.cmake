16d5807109c9118b4a176d7ed784600e7de0c73c
Super Genius <ken+github@gnus.ai>
2023-02-03 12:17:30 -0800

    Should fix using FMT external problem with SPDLOG

--- build/CommonBuildParameters.cmake
+++ build/CommonBuildParameters.cmake
@@ -95,6 +95,7 @@ set(spdlog_DIR "${_THIRDPARTY_BUILD_DIR}/spdlog/lib/cmake/spdlog")
 set(spdlog_INCLUDE_DIR "${_THIRDPARTY_BUILD_DIR}/spdlog/include")
 find_package(spdlog CONFIG REQUIRED)
 include_directories(${spdlog_INCLUDE_DIR})
+add_compile_definitions("SPDLOG_FMT_EXTERNAL")
 
 # --------------------------------------------------------
 # Set config of soralog
