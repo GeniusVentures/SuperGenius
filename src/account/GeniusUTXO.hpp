62143b354d4182547218482d3bcea7b2bff6c78c
Henrique A Klein <henryaklein@gmail.com>
2024-04-25 11:36:02 -0300

    WIP: GeniusUTXO created.

---
+++ src/account/GeniusUTXO.hpp
@@ -0,0 +1,38 @@
+/**
+ * @file       GeniusUTXO.hpp
+ * @brief      
+ * @date       2024-04-25
+ * @author     Henrique A. Klein (hklein@gnus.ai)
+ */
+#ifndef _GENIUS_UTXO_HPP
+#define _GENIUS_UTXO_HPP
+
+#include "base/blob.hpp"
+
+namespace sgns
+{
+    class GeniusUTXO
+    {
+    public:
+        GeniusUTXO( const base::Hash256 &hash, const uint32_t &previous_index, const uint64_t &amount ) :
+            txid_hash_( hash ),            //
+            output_idx_( previous_index ), //
+            amount_( amount ),             //
+            locked_( false )               //
+        {
+        }
+
+        void ToggleLock(bool lock)
+        {
+            locked_ = lock;
+        }
+
+    private:
+        base::Hash256 txid_hash_;
+        uint32_t      output_idx_;
+        uint64_t      amount_;
+        bool          locked_;
+    };
+}
+
+#endif
