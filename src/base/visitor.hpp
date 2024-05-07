873181f217e6617a2f28abd03c0bfff0d84e8999
Eduardo Menges Mattje <eduardomattje@gmail.com>
2024-05-07 15:04:48 -0300

    docs: Enhanced docs for visit_in_place

--- src/base/visitor.hpp
+++ src/base/visitor.hpp
@@ -1,40 +1,40 @@
 #ifndef SUPERGENIUS_VISITOR_HPP
 #define SUPERGENIUS_VISITOR_HPP
 
-#include <type_traits>  // for std::decay
-#include <utility>      // for std::forward
+#include <type_traits>
+#include <utility>
 
-#include <boost/variant/apply_visitor.hpp>  // for boost::apply_visitor
+#include <boost/variant/apply_visitor.hpp>
 
-namespace sgns {
+namespace sgns
+{
+    template <typename... Lambdas> struct lambda_visitor;
 
-  template <typename... Lambdas>
-  struct lambda_visitor;
+    template <typename Lambda, typename... Lambdas>
+    struct lambda_visitor<Lambda, Lambdas...> : public Lambda, public lambda_visitor<Lambdas...>
+    {
+        using Lambda::operator();
+        using lambda_visitor<Lambdas...>::operator();
 
-  template <typename Lambda, typename... Lambdas>
-  struct lambda_visitor<Lambda, Lambdas...>
-      : public Lambda, public lambda_visitor<Lambdas...> {
-    using Lambda::operator();
-    using lambda_visitor<Lambdas...>::operator();
+        lambda_visitor( Lambda lambda, Lambdas... lambdas ) : Lambda( lambda ), lambda_visitor<Lambdas...>( lambdas... )
+        {
+        }
+    };
 
-    // NOLINTNEXTLINE(google-explicit-constructor)
-    lambda_visitor(Lambda lambda, Lambdas... lambdas)
-        : Lambda(lambda), lambda_visitor<Lambdas...>(lambdas...) {}
-  };
+    template <typename Lambda> struct lambda_visitor<Lambda> : public Lambda
+    {
+        using Lambda::operator();
 
-  template <typename Lambda>
-  struct lambda_visitor<Lambda> : public Lambda {
-    using Lambda::operator();
+        lambda_visitor( Lambda lambda ) : Lambda( lambda )
+        {
+        }
+    };
 
-    // NOLINTNEXTLINE(google-explicit-constructor)
-    lambda_visitor(Lambda lambda) : Lambda(lambda) {}
-  };
-
-  /**
+    /**
    * @brief Convenient in-place compile-time visitor creation, from a set of
    * lambdas
    *
-   * @code
+   * @code{.cpp}
    * make_visitor([](int a){ return 1; },
    *              [](std::string b) { return 2; });
    * @nocode
@@ -48,50 +48,44 @@ namespace sgns {
    * }
    * @nocode
    *
-   * @param lambdas
    * @return visitor
    */
-  template <class... Fs>
-  constexpr auto make_visitor(Fs &&... fs) {
-    using visitor_type = lambda_visitor<std::decay_t<Fs>...>;
-    return visitor_type(std::forward<Fs>(fs)...);
-  }
+    template <class... Fs> constexpr auto make_visitor( Fs &&...fs )
+    {
+        using visitor_type = lambda_visitor<std::decay_t<Fs>...>;
+        return visitor_type( std::forward<Fs>( fs )... );
+    }
 
-  /**
-   * @brief Inplace visitor for boost::variant.
-   * @code
+    /**
+   * @brief Inplace visitor for boost::variant. Bascially poor man's pattern matching for variants.
+   * @code{.cpp}
    *   boost::variant<int, std::string> value = "1234";
-   *   ...
+   *
    *   visit_in_place(value,
    *                  [](int v) { std::cout << "(int)" << v; },
    *                  [](std::string v) { std::cout << "(string)" << v;}
    *                  );
    * @nocode
-   *
-   * @param variant
-   * @param lambdas
-   * @param lambdas
    */
-  template <typename TVariant, typename... TVisitors>
-  constexpr decltype(auto) visit_in_place(TVariant &&variant,
-                                          TVisitors &&... visitors) {
-    return boost::apply_visitor(
-        make_visitor(std::forward<TVisitors>(visitors)...),
-        std::forward<TVariant>(variant));
-  }
+    template <typename TVariant, typename... TVisitors>
+    constexpr decltype( auto ) visit_in_place( TVariant &&variant, TVisitors &&...visitors )
+    {
+        return boost::apply_visitor( make_visitor( std::forward<TVisitors>( visitors )... ),
+                                     std::forward<TVariant>( variant ) );
+    }
 
-  /// apply Matcher to optional T
-  template <typename T, typename Matcher>
-  constexpr decltype(auto) match(T &&t, Matcher &&m) {
-    return std::forward<T>(t) ? std::forward<Matcher>(m)(*std::forward<T>(t))
-                              : std::forward<Matcher>(m)();
-  }
+    /// apply Matcher to optional T
+    template <typename T, typename Matcher> constexpr decltype( auto ) match( T &&t, Matcher &&m )
+    {
+        return std::forward<T>( t ) ? std::forward<Matcher>( m )( *std::forward<T>( t ) )
+                                    : std::forward<Matcher>( m )();
+    }
 
-  /// construct visitor from Fs and apply it to optional T
-  template <typename T, typename... Fs>
-  constexpr decltype(auto) match_in_place(T &&t, Fs &&... fs) {
-    return match(std::forward<T>(t), make_visitor(std::forward<Fs>(fs)...));
-  }
-}  // namespace sgns
+    /// construct visitor from Fs and apply it to optional T
+    template <typename T, typename... Fs> constexpr decltype( auto ) match_in_place( T &&t, Fs &&...fs )
+    {
+        return match( std::forward<T>( t ), make_visitor( std::forward<Fs>( fs )... ) );
+    }
+}
 
-#endif  // SUPERGENIUS_VISITOR_HPP
+#endif
