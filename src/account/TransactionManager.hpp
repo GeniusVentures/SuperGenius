5ea70463379556a470d3e902e42ceac31f23d6a2
Eduardo Menges Mattje <eduardomattje@gmail.com>
2024-09-06 19:09:13 -0300

    Reformated `TransactionManager`

--- src/account/TransactionManager.hpp
+++ src/account/TransactionManager.hpp
@@ -32,20 +32,22 @@ namespace sgns
     {
     public:
         using ProcessFinishCbType = std::function<void( const std::string &, const std::set<std::string> & )>;
-        TransactionManager( std::shared_ptr<crdt::GlobalDB>           db,      //
-                            std::shared_ptr<boost::asio::io_context>  ctx,     //
-                            std::shared_ptr<GeniusAccount>            account, //
-                            std::shared_ptr<crypto::Hasher>           hasher,  //
+
+        TransactionManager( std::shared_ptr<crdt::GlobalDB>           db,
+                            std::shared_ptr<boost::asio::io_context>  ctx,
+                            std::shared_ptr<GeniusAccount>            account,
+                            std::shared_ptr<crypto::Hasher>           hasher,
                             std::shared_ptr<blockchain::BlockStorage> block_storage );
 
-        TransactionManager( std::shared_ptr<crdt::GlobalDB>           db,            //
-                            std::shared_ptr<boost::asio::io_context>  ctx,           //
-                            std::shared_ptr<GeniusAccount>            account,       //
-                            std::shared_ptr<crypto::Hasher>           hasher,        //
-                            std::shared_ptr<blockchain::BlockStorage> block_storage, //
+        TransactionManager( std::shared_ptr<crdt::GlobalDB>           db,
+                            std::shared_ptr<boost::asio::io_context>  ctx,
+                            std::shared_ptr<GeniusAccount>            account,
+                            std::shared_ptr<crypto::Hasher>           hasher,
+                            std::shared_ptr<blockchain::BlockStorage> block_storage,
                             ProcessFinishCbType                       processing_finished_cb );
 
         ~TransactionManager() = default;
+
         void Start();
         void PrintAccountInfo();
 
@@ -53,36 +55,18 @@ namespace sgns
 
         bool     TransferFunds( const uint256_t &amount, const uint256_t &destination );
         void     MintFunds( uint64_t amount );
-        bool     HoldEscrow(
-                uint64_t amount, uint64_t num_chunks, const uint256_t &dev_addr, float dev_cut, const std::string &job_id );
+        bool     HoldEscrow( uint64_t           amount,
+                             uint64_t           num_chunks,
+                             const uint256_t   &dev_addr,
+                             float              dev_cut,
+                             const std::string &job_id );
         bool     ReleaseEscrow( const std::string &job_id, const bool &pay );
         void     ProcessingDone( const std::string &subtask_id );
         uint64_t GetBalance();
 
     private:
-        std::shared_ptr<crdt::GlobalDB>                  db_m;
-        std::shared_ptr<boost::asio::io_context>         ctx_m;
-        std::shared_ptr<GeniusAccount>                   account_m;
-        std::deque<std::shared_ptr<IGeniusTransactions>> out_transactions;
-        std::shared_ptr<boost::asio::steady_timer>       timer_m;
-        mutable std::mutex                               mutex_m;
-        primitives::BlockNumber                          last_block_id_m;
-        std::uint64_t                                    last_trans_on_block_id;
-        std::shared_ptr<blockchain::BlockStorage>        block_storage_m;
-        std::shared_ptr<crypto::Hasher>                  hasher_m;
-        ProcessFinishCbType                              processing_finished_cb_m;
-
         struct EscrowCtrl
         {
-            uint256_t                                  dev_addr;
-            float                                      dev_cut;
-            uint256_t                                  job_hash;
-            uint256_t                                  full_amount;
-            uint64_t                                   num_subtasks;
-            InputUTXOInfo                              original_input;
-            std::vector<OutputDestInfo>                payout_peers;
-            std::unordered_map<std::string, uint256_t> subtask_info;
-
             EscrowCtrl( uint256_t     addr,
                         float         cut,
                         uint256_t     hash,
@@ -97,17 +81,19 @@ namespace sgns
                 original_input( std::move( input ) )
             {
             }
+
+            uint256_t                                  dev_addr;
+            float                                      dev_cut;
+            uint256_t                                  job_hash;
+            uint256_t                                  full_amount;
+            uint64_t                                   num_subtasks;
+            InputUTXOInfo                              original_input;
+            std::vector<OutputDestInfo>                payout_peers;
+            std::unordered_map<std::string, uint256_t> subtask_info;
         };
 
-        //TODO - Replace with std::unordered_map<std::string, EscrowCtrl> for better performance, maybe?
-        std::vector<EscrowCtrl> escrow_ctrl_m;
-        //Hash256                                          last_transaction_hash;
-
-        static constexpr std::uint16_t MAIN_NET_ID = 369;
-        static constexpr std::uint16_t TEST_NET_ID = 963;
-
-        base::Logger m_logger = sgns::base::createLogger( "TransactionManager" );
-
+        static constexpr std::uint16_t    MAIN_NET_ID             = 369;
+        static constexpr std::uint16_t    TEST_NET_ID             = 963;
         static constexpr std::string_view TRANSACTION_BASE_FORMAT = "bc-%hu/";
 
         void                     Update();
@@ -127,6 +113,24 @@ namespace sgns
          * @brief      Checks the blockchain for any new blocks to sync current values
          */
         void CheckBlockchain();
+
+        std::shared_ptr<crdt::GlobalDB>                  db_m;
+        std::shared_ptr<boost::asio::io_context>         ctx_m;
+        std::shared_ptr<GeniusAccount>                   account_m;
+        std::vector<std::vector<std::uint8_t>>           transactions;
+        std::deque<std::shared_ptr<IGeniusTransactions>> out_transactions;
+        std::shared_ptr<boost::asio::steady_timer>       timer_m;
+        mutable std::mutex                               mutex_m;
+        primitives::BlockNumber                          last_block_id_m;
+        std::uint64_t                                    last_trans_on_block_id;
+        std::shared_ptr<blockchain::BlockStorage>        block_storage_m;
+        std::shared_ptr<crypto::Hasher>                  hasher_m;
+        ProcessFinishCbType                              processing_finished_cb_m;
+
+        //TODO - Replace with std::unordered_map<std::string, EscrowCtrl> for better performance, maybe?
+        std::vector<EscrowCtrl> escrow_ctrl_m;
+
+        base::Logger m_logger = sgns::base::createLogger( "TransactionManager" );
     };
 }
 
