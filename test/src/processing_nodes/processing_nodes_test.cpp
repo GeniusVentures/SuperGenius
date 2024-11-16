a880c840a7333f973417460b3088b84306ca1b05
itsafuu <sariru@gmail.com>
2024-11-15 21:37:16 -0500

    Set up 3 nodes, assertion fails in kademlia(is it a singleton?)

--- test/src/processing_nodes/processing_nodes_test.cpp
+++ test/src/processing_nodes/processing_nodes_test.cpp
@@ -26,13 +26,16 @@ class ProcessingNodesTest : public ::testing::Test
 public:
     virtual void SetUp() override
     {
-
+        
     }
 };
+DevConfig_st DEV_CONFIG{ "0xcafe", 0.65, 1.0, 0 , "./node1"};
+DevConfig_st DEV_CONFIG2{ "0xcafe", 0.65, 1.0, 0 , "./node2"};
+DevConfig_st DEV_CONFIG3{ "0xcafe", 0.65, 1.0, 0 , "./node3"};
 
 TEST_F(ProcessingNodesTest, ProcessNodes)
 {
     sgns::GeniusNode node_main( DEV_CONFIG, "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef" );
-    sgns::GeniusNode node_proc1( DEV_CONFIG, "livebeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef" );
-    sgns::GeniusNode node_proc2( DEV_CONFIG, "zzombeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef" );
+    sgns::GeniusNode node_proc1( DEV_CONFIG2, "livebeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef" );
+    sgns::GeniusNode node_proc2( DEV_CONFIG3, "zzombeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef" );
 }
