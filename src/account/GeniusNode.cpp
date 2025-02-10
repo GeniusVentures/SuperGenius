6b223da48fe0a47b9c03ea799c6fca406ca182b4
Luiz Guilherme Rizzatto Zucchi <luizgrz@gmail.com>
2025-02-10 14:31:20 -0300

    feat: add token formatting and parsing utilities for Minion Tokens (1e-9 GNUS)

--- src/account/GeniusNode.cpp
+++ src/account/GeniusNode.cpp
@@ -403,7 +403,7 @@ namespace sgns
             //}
             //imageindex++;
         }
-        auto cut = sgns::fixed_point::fromString( dev_config_.Cut );
+            auto cut = sgns::fixed_point::fromString( dev_config_.Cut );
 
         if ( !cut )
         {
@@ -614,5 +614,15 @@ namespace sgns
     void GeniusNode::PrintDataStore()
     {
         globaldb_->PrintDataStore();
+    }
+    
+    std::string FormatTokens( uint64_t amount )
+    {
+        return sgns::fixed_point::toString( amount );
+    }
+
+    outcome::result<uint64_t> ParseTokens( const std::string &str )
+    {
+        return sgns::fixed_point::fromString( str );
     }
 }
