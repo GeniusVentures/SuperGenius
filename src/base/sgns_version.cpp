5074202fca85b67e18413a1678e9e095ea9d3e17
Luiz Guilherme Rizzatto Zucchi <luizgrz@gmail.com>
2025-02-25 20:13:41 -0300

    feat: git integration

--- src/base/sgns_version.cpp
+++ src/base/sgns_version.cpp
@@ -1,31 +1,40 @@
+/**
+ * @file sgns_version.cpp
+ * @brief Implementation of version retrieval functions for SuperGenius.
+ *
+ * This file provides functions to retrieve version information for SuperGenius.
+ * All version-related macros (e.g. SUPERGENIUS_VERSION_NUMBER) are defined via CMake.
+ *
+ */
+#include "sgnsv.h"
 #include "sgns_version.hpp"
 
-unsigned long SuperGenius_version_num(void)
+uint64_t sgns::version::SuperGeniusVersionNum()
 {
     return SUPERGENIUS_VERSION_NUMBER;
 }
 
-unsigned int SuperGenius_version_major(void)
+uint32_t sgns::version::SuperGeniusVersionMajor()
 {
     return SUPERGENIUS_VERSION_MAJOR;
 }
 
-unsigned int SuperGenius_version_minor(void)
+uint32_t sgns::version::SuperGeniusVersionMinor()
 {
     return SUPERGENIUS_VERSION_MINOR;
 }
 
-unsigned int SuperGenius_version_patch(void)
+uint32_t sgns::version::SuperGeniusVersionPatch()
 {
     return SUPERGENIUS_VERSION_PATCH;
 }
 
-const char *SuperGenius_version_pre_release(void)
+const char *sgns::version::SuperGeniusVersionPreRelease()
 {
     return SUPERGENIUS_VERSION_PRE_RELEASE;
 }
 
-const char *SuperGenius_version_build_metadata(void)
+const char *sgns::version::SuperGeniusVersionBuildMetadata()
 {
     return SUPERGENIUS_VERSION_BUILD_METADATA;
 }
---
+++ src/base/sgns_version.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cstdint>
+namespace sgns
+{
+    namespace version
+    {
+        /**
+         * @brief Retrieves the complete version number of SuperGenius.
+         *
+         * @return uint64_t representing the version number.
+         */
+        uint64_t SuperGeniusVersionNum();
+
+        /**
+         * @brief Retrieves the major version of SuperGenius.
+         *
+         * @return uint32_t representing the major version.
+         */
+        uint32_t SuperGeniusVersionMajor();
+
+        /**
+         * @brief Retrieves the minor version of SuperGenius.
+         *
+         * @return uint32_t representing the minor version.
+         */
+        uint32_t SuperGeniusVersionMinor();
+
+        /**
+         * @brief Retrieves the patch version of SuperGenius.
+         *
+         * @return uint32_t representing the patch version.
+         */
+        uint32_t SuperGeniusVersionPatch();
+
+        /**
+         * @brief Retrieves the pre-release identifier of SuperGenius.
+         *
+         * @return const char* pointing to the pre-release string.
+         */
+        const char *SuperGeniusVersionPreRelease();
+
+        /**
+         * @brief Retrieves the build metadata of SuperGenius.
+         *
+         * @return const char* pointing to the build metadata string.
+         */
+        const char *SuperGeniusVersionBuildMetadata();
+    }
+}
