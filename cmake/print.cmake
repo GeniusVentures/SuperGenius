5b3354d0a421a6a2032bba36382c9fd56e527add
Eduardo Menges Mattje <eduardomattje@gmail.com>
2025-03-07 10:56:37 -0300

    Sped up compile times

--- cmake/print.cmake
+++
@@ -1,7 +0,0 @@
-function(print)
-    message(STATUS "[${CMAKE_PROJECT_NAME}] ${ARGV}")
-endfunction()
-
-function(fatal_error)
-    message(FATAL_ERROOR "[${CMAKE_PROJECT_NAME}] ${ARGV}")
-endfunction()
