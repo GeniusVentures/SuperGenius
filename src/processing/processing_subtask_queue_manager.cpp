2ffa383414ace58a90ce470062bc7b687a76b2b6
Henrique A Klein <henryaklein@gmail.com>
2025-03-07 14:18:51 -0300

    Feat: Increased subtask timeout to be able to payout before someone takes over

--- src/processing/processing_subtask_queue_manager.cpp
+++ src/processing/processing_subtask_queue_manager.cpp
@@ -17,7 +17,7 @@ namespace sgns::processing
         m_queueResponseTimeout( boost::posix_time::seconds( 5 ) ),
         m_dltGrabSubTaskTimeout( *m_context ),
         m_processingQueue( localNodeId ),
-        m_processingTimeout( std::chrono::seconds( 10 ) ),
+        m_processingTimeout( std::chrono::seconds( 15 ) ),
         m_processingErrorSink( std::move( processingErrorSink ) ),
         m_queue_timestamp_( 0 ),
         m_ownership_acquired_at_( 0 )
