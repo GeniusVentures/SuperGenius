6a6ac67caf749e9494941600d62a4d7b9cd16766
Luiz Guilherme Rizzatto Zucchi <luizgrz@gmail.com>
2025-03-11 20:29:36 -0300

    test: refactoring and creating the test where the receiver not get the transaction

--- test/src/account/transaction_error_test.cpp
+++ test/src/account/transaction_error_test.cpp
@@ -1,59 +1,63 @@
 #include <gtest/gtest.h>
-#include <cmath>
-#include <fstream>
 #include <memory>
 #include <iostream>
-#include <cstdint>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 #ifdef _WIN32
 //#include <windows.h>
 #else
 #include <termios.h>
 #include <unistd.h>
 #endif
-#include <functional>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <boost/program_options.hpp>
-#include <boost/format.hpp>
-#include <boost/asio.hpp>
-#include <boost/multiprecision/cpp_int.hpp>
-#include "local_secure_storage/impl/json/JSONSecureStorage.hpp"
 #include "account/GeniusNode.hpp"
 #include "FileManager.hpp"
 #include <boost/dll.hpp>
-#include <boost/algorithm/string/replace.hpp>
 
+/**
+ * @class TestTransactionManagerCallbacks
+ * @brief Controls transaction execution by pausing and resuming.
+ */
 class TestTransactionManagerCallbacks : public sgns::TransactionManagerCallbacks
 {
 public:
+    /**
+     * @brief Blocks transaction execution until resumed.
+     */
     void onEnterSendTransaction() override
     {
         std::unique_lock<std::mutex> lock( mutex_ );
-        cv_.wait( lock, [this] { return resumed_; } );
+        cv_.wait( lock, [this] { return resumed_.load(); } );
     }
 
+    /**
+     * @brief Pauses transaction execution.
+     */
     void pause()
     {
         std::lock_guard<std::mutex> lock( mutex_ );
-        resumed_ = false;
+        resumed_.store( false );
         std::cout << "Pausing transaction sending." << std::endl;
     }
 
+    /**
+     * @brief Resumes transaction execution.
+     */
     void resume()
     {
         {
             std::lock_guard<std::mutex> lock( mutex_ );
-            resumed_ = true;
+            resumed_.store( true );
         }
         cv_.notify_all();
         std::cout << "Resuming transaction sending." << std::endl;
     }
 
 private:
-    std::mutex              mutex_;
-    std::condition_variable cv_;
-    bool                    resumed_ = true;
+    std::mutex              mutex_;           ///< Protects condition variable.
+    std::condition_variable cv_;              ///< Blocks transactions when paused.
+    std::atomic<bool>       resumed_{ true }; ///< Controls transaction state.
 };
 
 class TransactionRecoveryTest : public ::testing::Test
@@ -61,7 +65,8 @@ class TransactionRecoveryTest : public ::testing::Test
 protected:
     static inline sgns::GeniusNode                                *node_proc1 = nullptr;
     static inline sgns::GeniusNode                                *node_proc2 = nullptr;
-    static inline std::shared_ptr<TestTransactionManagerCallbacks> testTxCallbacks;
+    static inline std::shared_ptr<TestTransactionManagerCallbacks> node1Callbacks;
+    static inline std::shared_ptr<TestTransactionManagerCallbacks> node2Callbacks;
     static inline DevConfig_st DEV_CONFIG  = { "0xcafe", "0.65", 1.0, 0, "./node21" };
     static inline DevConfig_st DEV_CONFIG2 = { "0xcafe", "0.65", 1.0, 0, "./node22" };
     static inline std::string  binary_path = "";
@@ -88,8 +93,10 @@ protected:
         std::this_thread::sleep_for( std::chrono::milliseconds( 1000 ) );
         node_proc1->GetPubSub()->AddPeers( { node_proc2->GetPubSub()->GetLocalAddress() } );
         node_proc2->GetPubSub()->AddPeers( { node_proc1->GetPubSub()->GetLocalAddress() } );
-        testTxCallbacks = std::make_shared<TestTransactionManagerCallbacks>();
-        node_proc1->setTransactionCallbacks( testTxCallbacks );
+        node1Callbacks = std::make_shared<TestTransactionManagerCallbacks>();
+        node_proc1->setTransactionCallbacks( node1Callbacks );
+        node2Callbacks = std::make_shared<TestTransactionManagerCallbacks>();
+        node_proc2->setTransactionCallbacks( node2Callbacks );
     }
 
     static void TearDownTestSuite()
@@ -105,11 +112,23 @@ protected:
             node_proc2 = nullptr;
         }
     }
+
+    void TearDown() override
+    {
+        if ( node1Callbacks )
+        {
+            node1Callbacks->resume();
+        }
+        if ( node2Callbacks )
+        {
+            node2Callbacks->resume();
+        }
+    }
 };
 
 TEST_F( TransactionRecoveryTest, MintTransactionLowTimeout )
 {
-    testTxCallbacks->pause();
+    node1Callbacks->pause();
     auto balance_before       = node_proc1->GetBalance();
     auto out_txs_before       = node_proc1->GetOutTransactions();
     auto out_txs_count_before = out_txs_before.size();
@@ -118,24 +137,21 @@ TEST_F( TransactionRecoveryTest, MintTransactionLowTimeout )
     std::this_thread::sleep_for( std::chrono::milliseconds( OUTGOING_TIMEOUT_MILLISECONDS ) );
     EXPECT_EQ( node_proc1->GetOutTransactions().size(), out_txs_count_before );
     EXPECT_EQ( node_proc1->GetBalance(), balance_before );
-    testTxCallbacks->resume();
-    std::this_thread::sleep_for( std::chrono::milliseconds( 2000 ) );
 }
 
 TEST_F( TransactionRecoveryTest, MintAfterTimeoutFailure )
 {
-    testTxCallbacks->pause();
+    node1Callbacks->pause();
     auto balance_before       = node_proc1->GetBalance();
     auto out_txs_before       = node_proc1->GetOutTransactions();
     auto out_txs_count_before = out_txs_before.size();
     auto mint_result          = node_proc1->MintTokens( 100000000000, "", "", "", std::chrono::milliseconds( 1 ) );
     ASSERT_FALSE( mint_result.has_value() );
-    testTxCallbacks->resume();
+    node1Callbacks->resume();
     mint_result = node_proc1->MintTokens( 1, "", "", "", std::chrono::milliseconds( OUTGOING_TIMEOUT_MILLISECONDS ) );
     ASSERT_TRUE( mint_result.has_value() );
     EXPECT_EQ( node_proc1->GetOutTransactions().size(), out_txs_count_before + 1 );
     EXPECT_EQ( node_proc1->GetBalance(), balance_before + 1 );
-    std::this_thread::sleep_for( std::chrono::milliseconds( 2000 ) );
 }
 
 TEST_F( TransactionRecoveryTest, TransferTransactionLowTimeout )
@@ -146,7 +162,7 @@ TEST_F( TransactionRecoveryTest, TransferTransactionLowTimeout )
                                                "",
                                                std::chrono::milliseconds( OUTGOING_TIMEOUT_MILLISECONDS ) );
     ASSERT_TRUE( mint_result.has_value() );
-    testTxCallbacks->pause();
+    node1Callbacks->pause();
     auto node1_balance_before = node_proc1->GetBalance();
     auto node2_balance_before = node_proc2->GetBalance();
     auto node1_out_txs_before = node_proc1->GetOutTransactions().size();
@@ -158,8 +174,6 @@ TEST_F( TransactionRecoveryTest, TransferTransactionLowTimeout )
     EXPECT_EQ( node_proc2->GetBalance(), node2_balance_before );
     EXPECT_EQ( node_proc1->GetBalance(), node1_balance_before );
     EXPECT_EQ( node_proc1->GetOutTransactions().size(), node1_out_txs_before );
-    std::this_thread::sleep_for( std::chrono::milliseconds( 2000 ) );
-    testTxCallbacks->resume();
 }
 
 TEST_F( TransactionRecoveryTest, TransferAfterTimeoutFailure )
@@ -170,7 +184,7 @@ TEST_F( TransactionRecoveryTest, TransferAfterTimeoutFailure )
                                                "",
                                                std::chrono::milliseconds( OUTGOING_TIMEOUT_MILLISECONDS ) );
     ASSERT_TRUE( mint_result.has_value() );
-    testTxCallbacks->pause();
+    node1Callbacks->pause();
     auto node1_balance_before = node_proc1->GetBalance();
     auto node2_balance_before = node_proc2->GetBalance();
     auto node1_out_txs_before = node_proc1->GetOutTransactions().size();
@@ -179,7 +193,7 @@ TEST_F( TransactionRecoveryTest, TransferAfterTimeoutFailure )
                                                       node_proc2->GetAddress(),
                                                       std::chrono::milliseconds( 1 ) );
     ASSERT_FALSE( transfer_result.has_value() );
-    testTxCallbacks->resume();
+    node1Callbacks->resume();
     transfer_result = node_proc1->TransferFunds( 1,
                                                  node_proc2->GetAddress(),
                                                  std::chrono::milliseconds( OUTGOING_TIMEOUT_MILLISECONDS ) );
@@ -189,6 +203,50 @@ TEST_F( TransactionRecoveryTest, TransferAfterTimeoutFailure )
         transfer_tx_id,
         std::chrono::milliseconds( INCOMING_TIMEOUT_MILLISECONDS ) );
     ASSERT_TRUE( transfer_received );
+    EXPECT_EQ( node_proc1->GetBalance(), node1_balance_before - 1 );
+    EXPECT_EQ( node_proc2->GetBalance(), node2_balance_before + 1 );
+    EXPECT_EQ( node_proc1->GetOutTransactions().size(), node1_out_txs_before + 1 );
+    EXPECT_EQ( node_proc2->GetInTransactions().size(), node2_in_txs_before + 1 );
+}
+
+TEST_F( TransactionRecoveryTest, TransferWithReceiverStopped )
+{
+    node2Callbacks->pause();
+
+    auto mint_result = node_proc1->MintTokens( 1500000000,
+                                               "",
+                                               "",
+                                               "",
+                                               std::chrono::milliseconds( OUTGOING_TIMEOUT_MILLISECONDS ) );
+    ASSERT_TRUE( mint_result.has_value() );
+
+    auto node1_balance_before = node_proc1->GetBalance();
+    auto node2_balance_before = node_proc2->GetBalance();
+    auto node1_out_txs_before = node_proc1->GetOutTransactions().size();
+    auto node2_in_txs_before  = node_proc2->GetInTransactions().size();
+
+    auto transfer_result = node_proc1->TransferFunds( 1,
+                                                      node_proc2->GetAddress(),
+                                                      std::chrono::milliseconds( OUTGOING_TIMEOUT_MILLISECONDS ) );
+    ASSERT_TRUE( transfer_result.has_value() );
+    auto [transfer_tx_id, transfer_duration] = transfer_result.value();
+
+    bool transfer_received = node_proc2->WaitForTransactionIncoming(
+        transfer_tx_id,
+        std::chrono::milliseconds( INCOMING_TIMEOUT_MILLISECONDS ) );
+    ASSERT_FALSE( transfer_received );
+    EXPECT_EQ( node_proc1->GetBalance(), node1_balance_before - 1 );
+    EXPECT_EQ( node_proc2->GetBalance(), node2_balance_before );
+    EXPECT_EQ( node_proc1->GetOutTransactions().size(), node1_out_txs_before + 1 );
+    EXPECT_EQ( node_proc2->GetInTransactions().size(), node2_in_txs_before );
+
+    node2Callbacks->resume();
+
+    transfer_received = node_proc2->WaitForTransactionIncoming(
+        transfer_tx_id,
+        std::chrono::milliseconds( INCOMING_TIMEOUT_MILLISECONDS ) );
+    ASSERT_TRUE( transfer_received );
+
     EXPECT_EQ( node_proc1->GetBalance(), node1_balance_before - 1 );
     EXPECT_EQ( node_proc2->GetBalance(), node2_balance_before + 1 );
     EXPECT_EQ( node_proc1->GetOutTransactions().size(), node1_out_txs_before + 1 );
