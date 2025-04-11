03ceb79a393f70745f66acd91093124a399dd9c1
Luiz Guilherme Rizzatto Zucchi <luizgrz@gmail.com>
2025-04-11 11:43:58 -0300

    fix: getting m_pubsub from m_pubsub when constructor without broadcaster is used

--- src/crdt/globaldb/globaldb.cpp
+++ src/crdt/globaldb/globaldb.cpp
@@ -250,6 +250,28 @@ std::string GetLocalIP( boost::asio::io_context &io )
         }
 
         auto ipfsDataStore = std::make_shared<RocksdbDatastore>( ipfsDBResult.value() );
+        auto scheduler = std::make_shared<libp2p::protocol::AsioScheduler>( m_context,
+                                                                            libp2p::protocol::SchedulerConfig{} );
+
+        std::shared_ptr<libp2p::Host> host;
+        if ( m_broadcastChannel )
+        {
+            host = m_broadcastChannel->GetPubsub()->GetHost();
+        }
+        else if ( m_pubsub )
+        {
+            host = m_pubsub->GetHost();
+        }
+        else
+        {
+            m_logger->error( "Neither broadcast channel nor pubsub is initialized." );
+            return outcome::failure( Error::DAG_SYNCHER_NOT_LISTENING );
+        }
+
+        auto graphsync = std::make_shared<GraphsyncImpl>( host, std::move( scheduler ) );
+        auto dagSyncer = std::make_shared<GraphsyncDAGSyncer>( ipfsDataStore,
+                                                               graphsync,
+                                                               host );
 
         auto dagSyncerHost = injector.create<std::shared_ptr<libp2p::Host>>();
 
