b77926b2d37f5e940da5d9cdfdfa0c89cf120065
Eduardo Menges Mattje <eduardomattje@gmail.com>
2025-05-02 15:45:44 -0300

    Fixed OpenSSL

--- build/CommonBuildParameters.cmake
+++ build/CommonBuildParameters.cmake
@@ -101,10 +101,12 @@ elseif(CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
 endif()
 
 # OpenSSL
-set(OPENSSL_DIR "${_THIRDPARTY_BUILD_DIR}/openssl/build/${ABI_SUBFOLDER_NAME}" CACHE PATH "Path to OpenSSL install folder")
+set(OPENSSL_DIR "${_THIRDPARTY_BUILD_DIR}/openssl/build" CACHE PATH "Path to OpenSSL install folder")
 set(OPENSSL_USE_STATIC_LIBS ON CACHE BOOL "OpenSSL use static libs")
 set(OPENSSL_MSVC_STATIC_RT ON CACHE BOOL "OpenSSL use static RT")
 set(OPENSSL_ROOT_DIR "${OPENSSL_DIR}" CACHE PATH "Path to OpenSSL install root folder")
+set(OPENSSL_INCLUDE_DIR "${OPENSSL_DIR}/include" CACHE PATH "Path to OpenSSL include folder")
+
 find_package(OpenSSL REQUIRED)
 
 # rocksdb
