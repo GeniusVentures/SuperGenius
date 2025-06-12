7bd8bc4f90400fe5206a3ae0d745623405713ec2
Luiz Guilherme Rizzatto Zucchi <luizgrz@gmail.com>
2025-06-12 18:46:59 -0300

    fix: token value in migration test

--- test/src/transaction_sync/migration_sync_test.cpp
+++ test/src/transaction_sync/migration_sync_test.cpp
@@ -29,7 +29,7 @@ protected:
     static inline DevConfig_st DEV_CONFIG = {
         "0xcafe", // Addr
         "0.65",   // Cut
-        1.0,      // TokenValueInGNUS
+        "1.0",    // TokenValueInGNUS
         0,        // TokenID
         ""        // BaseWritePath
     };
