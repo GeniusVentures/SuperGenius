e7d9d5be6cbbf5b8b348b435b28476f08f412ef9
Henrique A Klein <henryaklein@gmail.com>
2025-06-23 16:54:31 -0300

    Fix: Scope of lock_guards and extra one on PrimeCache

--- src/crdt/impl/crdt_heads.cpp
+++ src/crdt/impl/crdt_heads.cpp
@@ -13,229 +13,235 @@ namespace sgns::crdt
         auto result = this->PrimeCache();
     }
 
-  CrdtHeads::CrdtHeads(const CrdtHeads& aHeads)
-  {
-    *this = aHeads;
-  }
-
-  CrdtHeads& CrdtHeads::operator=(const CrdtHeads& aHeads)
-  {
-    if (this != &aHeads)
+    CrdtHeads::CrdtHeads( const CrdtHeads &aHeads )
     {
-      this->dataStore_ = aHeads.dataStore_;
-      this->namespaceKey_ = aHeads.namespaceKey_;
-      this->cache_ = aHeads.cache_;
-    }
-    return *this;
-  }
-
-  bool CrdtHeads::operator==(const CrdtHeads& aHeads)
-  {
-    bool returnEqual = true;
-    returnEqual &= this->dataStore_ == aHeads.dataStore_;
-    returnEqual &= this->namespaceKey_ == aHeads.namespaceKey_;
-    returnEqual &= this->cache_ == aHeads.cache_;
-    return returnEqual;
-  }
-
-  bool CrdtHeads::operator!=(const CrdtHeads& aHeads)
-  {
-    return !(*this == aHeads);
-  }
-
-  HierarchicalKey CrdtHeads::GetNamespaceKey() const
-  {
-    return this->namespaceKey_;
-  }
-
-
-  outcome::result<HierarchicalKey> CrdtHeads::GetKey(const CID& aCid)
-  {
-    // /<namespace>/<cid>
-    auto cidToStringResult = aCid.toString();
-    if (cidToStringResult.has_failure())
-    {
-      return outcome::failure(cidToStringResult.error());
+        *this = aHeads;
     }
 
-    return this->namespaceKey_.ChildString(cidToStringResult.value());
-  }
-
-  outcome::result<void> CrdtHeads::Write( const std::unique_ptr<storage::BufferBatch> &aDataStore,
-                                          const CID                                   &aCid,
-                                          uint64_t                                     aHeight )
-  {
-    if (aDataStore == nullptr)
+    CrdtHeads &CrdtHeads::operator=( const CrdtHeads &aHeads )
     {
-      return outcome::failure(boost::system::error_code{});
+        if ( this != &aHeads )
+        {
+            this->dataStore_    = aHeads.dataStore_;
+            this->namespaceKey_ = aHeads.namespaceKey_;
+            this->cache_        = aHeads.cache_;
+        }
+        return *this;
     }
 
-    auto getKeyResult = this->GetKey(aCid);
-    if (getKeyResult.has_failure())
+    bool CrdtHeads::operator==( const CrdtHeads &aHeads )
     {
-      return outcome::failure(getKeyResult.error());
+        bool returnEqual  = true;
+        returnEqual      &= this->dataStore_ == aHeads.dataStore_;
+        returnEqual      &= this->namespaceKey_ == aHeads.namespaceKey_;
+        returnEqual      &= this->cache_ == aHeads.cache_;
+        return returnEqual;
     }
 
-    std::string strHeight = std::to_string( aHeight );
-
-    Buffer keyBuffer;
-    keyBuffer.put(getKeyResult.value().GetKey());
-
-    Buffer valueBuffer;
-    valueBuffer.put(strHeight);
-
-    return aDataStore->put(keyBuffer, valueBuffer);
-  }
-
-  outcome::result<void> CrdtHeads::Delete(const std::unique_ptr<storage::BufferBatch>& aDataStore, const CID& aCid)
-  {
-    if (aDataStore == nullptr)
+    bool CrdtHeads::operator!=( const CrdtHeads &aHeads )
     {
-      return outcome::failure(boost::system::error_code{});
+        return !( *this == aHeads );
     }
 
-    auto getKeyResult = this->GetKey(aCid);
-    if (getKeyResult.has_failure())
+    HierarchicalKey CrdtHeads::GetNamespaceKey() const
     {
-      return outcome::failure(getKeyResult.error());
+        return this->namespaceKey_;
     }
 
-    Buffer keyBuffer;
-    keyBuffer.put(getKeyResult.value().GetKey());
-
-    return aDataStore->remove(keyBuffer);
-  }
-
-  bool CrdtHeads::IsHead(const CID& aCid)
-  {
-    // IsHead returns if a given cid is among the current heads.
-    std::lock_guard lg(this->mutex_);
-    return cache_.find(aCid) != cache_.end();
-  }
-
-  outcome::result<uint64_t> CrdtHeads::GetHeadHeight(const CID& aCid)
-  {
-    std::lock_guard lg(this->mutex_);
-
-    if (!this->IsHead(aCid))
+    outcome::result<HierarchicalKey> CrdtHeads::GetKey( const CID &aCid )
     {
-        return 0;
-    }
-    return this->cache_[aCid];
-  }
+        // /<namespace>/<cid>
+        auto cidToStringResult = aCid.toString();
+        if ( cidToStringResult.has_failure() )
+        {
+            return outcome::failure( cidToStringResult.error() );
+        }
 
-  outcome::result<int> CrdtHeads::GetLength()
-  {
-    std::lock_guard lg(this->mutex_);
-    return this->cache_.size();
-  }
-
-  outcome::result<void> CrdtHeads::Add( const CID &aCid, uint64_t aHeight )
-  {
-    if (this->dataStore_ == nullptr)
-    {
-      return outcome::failure(boost::system::error_code{});
+        return this->namespaceKey_.ChildString( cidToStringResult.value() );
     }
 
-    auto batchDatastore = this->dataStore_->batch();
-    auto writeResult = this->Write(batchDatastore, aCid, aHeight);
-    if (writeResult.has_failure())
+    outcome::result<void> CrdtHeads::Write( const std::unique_ptr<storage::BufferBatch> &aDataStore,
+                                            const CID                                   &aCid,
+                                            uint64_t                                     aHeight )
     {
-      return outcome::failure(writeResult.error());
-    }
-    auto commitResult = batchDatastore->commit();
-    if (commitResult.has_failure())
-    {
-      return outcome::failure(commitResult.error());
-    }
+        if ( aDataStore == nullptr )
+        {
+            return outcome::failure( boost::system::error_code{} );
+        }
 
-    std::lock_guard lg(this->mutex_);
-    this->cache_[aCid] = aHeight;
-    return outcome::success();
-  }
+        auto getKeyResult = this->GetKey( aCid );
+        if ( getKeyResult.has_failure() )
+        {
+            return outcome::failure( getKeyResult.error() );
+        }
 
-  outcome::result<void> CrdtHeads::Replace( const CID &aCidHead, const CID &aNewHeadCid, uint64_t aHeight )
-  {
-    if (this->dataStore_ == nullptr)
-    {
-      return outcome::failure(boost::system::error_code{});
-    }
+        std::string strHeight = std::to_string( aHeight );
 
-    auto batchDatastore = this->dataStore_->batch();
-    auto writeResult = this->Write(batchDatastore, aNewHeadCid, aHeight);
-    if (writeResult.has_failure())
-    {
-      return outcome::failure(writeResult.error());
+        Buffer keyBuffer;
+        keyBuffer.put( getKeyResult.value().GetKey() );
+
+        Buffer valueBuffer;
+        valueBuffer.put( strHeight );
+
+        return aDataStore->put( keyBuffer, valueBuffer );
     }
 
-    auto deleteResult = this->Delete(batchDatastore, aCidHead);
-    if (deleteResult.has_failure())
+    outcome::result<void> CrdtHeads::Delete( const std::unique_ptr<storage::BufferBatch> &aDataStore, const CID &aCid )
     {
-      return outcome::failure(deleteResult.error());
+        if ( aDataStore == nullptr )
+        {
+            return outcome::failure( boost::system::error_code{} );
+        }
+
+        auto getKeyResult = this->GetKey( aCid );
+        if ( getKeyResult.has_failure() )
+        {
+            return outcome::failure( getKeyResult.error() );
+        }
+
+        Buffer keyBuffer;
+        keyBuffer.put( getKeyResult.value().GetKey() );
+
+        return aDataStore->remove( keyBuffer );
     }
 
-    auto commitResult = batchDatastore->commit();
-    if (commitResult.has_failure())
+    bool CrdtHeads::IsHead( const CID &aCid )
     {
-      return outcome::failure(commitResult.error());
+        // IsHead returns if a given cid is among the current heads.
+        std::lock_guard lg( this->mutex_ );
+        return cache_.find( aCid ) != cache_.end();
     }
 
-    std::lock_guard lg(this->mutex_);
-    this->cache_.erase(aCidHead);
-    this->cache_[aNewHeadCid] = aHeight;
-    return outcome::success();
-  }
-
-  outcome::result<void> CrdtHeads::GetList(std::vector<CID>& aHeads, uint64_t& aMaxHeight)
-  {
-    std::lock_guard lg(this->mutex_);
-    aMaxHeight = 0;
-    aHeads.clear(); 
-    for (auto it = this->cache_.begin(); it != this->cache_.end(); ++it)
+    outcome::result<uint64_t> CrdtHeads::GetHeadHeight( const CID &aCid )
     {
-      aHeads.push_back(it->first);
-      aMaxHeight = std::max(aMaxHeight, it->second);
-    }
-    return outcome::success();
-  }
+        std::lock_guard lg( this->mutex_ );
 
-  outcome::result<void> CrdtHeads::PrimeCache()
-  {
-    // builds the heads cache based on what's in storage
-    const auto strNamespace = this->namespaceKey_.GetKey();
-    Buffer keyPrefixBuffer;
-    keyPrefixBuffer.put(strNamespace);
-    auto queryResult = this->dataStore_->query(keyPrefixBuffer);
-    if (queryResult.has_failure())
+        if ( !this->IsHead( aCid ) )
+        {
+            return 0;
+        }
+        return this->cache_[aCid];
+    }
+
+    outcome::result<int> CrdtHeads::GetLength()
     {
-      return outcome::failure(queryResult.error());
+        std::lock_guard lg( this->mutex_ );
+        return this->cache_.size();
     }
-    
-    for (const auto& bufferKeyAndValue : queryResult.value())
+
+    outcome::result<void> CrdtHeads::Add( const CID &aCid, uint64_t aHeight )
     {
-      std::string keyWithNamespace = std::string(bufferKeyAndValue.first.toString());
-      std::string strCid = keyWithNamespace.erase(0, strNamespace.size() + 1);
+        if ( this->dataStore_ == nullptr )
+        {
+            return outcome::failure( boost::system::error_code{} );
+        }
 
-      auto cidResult = CID::fromString(strCid);
-      if (cidResult.has_failure())
-      {
-        continue;
-      }
-      CID cid(cidResult.value());
+        auto batchDatastore = this->dataStore_->batch();
+        auto writeResult    = this->Write( batchDatastore, aCid, aHeight );
+        if ( writeResult.has_failure() )
+        {
+            return outcome::failure( writeResult.error() );
+        }
+        auto commitResult = batchDatastore->commit();
+        if ( commitResult.has_failure() )
+        {
+            return outcome::failure( commitResult.error() );
+        }
 
-      uint64_t height = 0;
-      try
-      {
-        height = boost::lexical_cast<uint64_t>(bufferKeyAndValue.second.toString());
-      }
-      catch (boost::bad_lexical_cast&)
-      {
-        return outcome::failure(boost::system::error_code{});
-      }
-
-      this->cache_[cid] = height;
+        {
+            std::lock_guard lg( this->mutex_ );
+            this->cache_[aCid] = aHeight;
+        }
+        return outcome::success();
     }
-    return outcome::success();
-  }
+
+    outcome::result<void> CrdtHeads::Replace( const CID &aCidHead, const CID &aNewHeadCid, uint64_t aHeight )
+    {
+        if ( this->dataStore_ == nullptr )
+        {
+            return outcome::failure( boost::system::error_code{} );
+        }
+
+        auto batchDatastore = this->dataStore_->batch();
+        auto writeResult    = this->Write( batchDatastore, aNewHeadCid, aHeight );
+        if ( writeResult.has_failure() )
+        {
+            return outcome::failure( writeResult.error() );
+        }
+
+        auto deleteResult = this->Delete( batchDatastore, aCidHead );
+        if ( deleteResult.has_failure() )
+        {
+            return outcome::failure( deleteResult.error() );
+        }
+
+        auto commitResult = batchDatastore->commit();
+        if ( commitResult.has_failure() )
+        {
+            return outcome::failure( commitResult.error() );
+        }
+
+        {
+            std::lock_guard lg( this->mutex_ );
+            this->cache_.erase( aCidHead );
+            this->cache_[aNewHeadCid] = aHeight;
+        }
+        return outcome::success();
+    }
+
+    outcome::result<void> CrdtHeads::GetList( std::vector<CID> &aHeads, uint64_t &aMaxHeight )
+    {
+        std::lock_guard lg( this->mutex_ );
+        aMaxHeight = 0;
+        aHeads.clear();
+        for ( auto it = this->cache_.begin(); it != this->cache_.end(); ++it )
+        {
+            aHeads.push_back( it->first );
+            aMaxHeight = std::max( aMaxHeight, it->second );
+        }
+        return outcome::success();
+    }
+
+    outcome::result<void> CrdtHeads::PrimeCache()
+    {
+        // builds the heads cache based on what's in storage
+
+        const auto strNamespace = this->namespaceKey_.GetKey();
+        Buffer     keyPrefixBuffer;
+        keyPrefixBuffer.put( strNamespace );
+        auto queryResult = this->dataStore_->query( keyPrefixBuffer );
+        if ( queryResult.has_failure() )
+        {
+            return outcome::failure( queryResult.error() );
+        }
+
+        for ( const auto &bufferKeyAndValue : queryResult.value() )
+        {
+            std::string keyWithNamespace = std::string( bufferKeyAndValue.first.toString() );
+            std::string strCid           = keyWithNamespace.erase( 0, strNamespace.size() + 1 );
+
+            auto cidResult = CID::fromString( strCid );
+            if ( cidResult.has_failure() )
+            {
+                continue;
+            }
+            CID cid( cidResult.value() );
+
+            uint64_t height = 0;
+            try
+            {
+                height = boost::lexical_cast<uint64_t>( bufferKeyAndValue.second.toString() );
+            }
+            catch ( boost::bad_lexical_cast & )
+            {
+                return outcome::failure( boost::system::error_code{} );
+            }
+            {
+                std::lock_guard lg( this->mutex_ );
+                this->cache_[cid] = height;
+            }
+        }
+        return outcome::success();
+    }
 
 }
