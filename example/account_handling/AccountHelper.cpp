aa8411c1bc0c3fe06e32e31d63ca2279c858f25e
Eduardo Menges Mattje <eduardomattje@gmail.com>
2026-01-21 16:58:20 -0300

    Fixed CI

--- example/account_handling/AccountHelper.cpp
+++ example/account_handling/AccountHelper.cpp
@@ -121,7 +121,7 @@ namespace sgns
             throw std::runtime_error( "Error initializing blockchain" );
         }
         block_storage_       = std::move( maybe_block_storage.value() );
-        transaction_manager_ = TransactionManager::New( globaldb_, io_, &utxo_manager_, account_, hasher_ );
+        transaction_manager_ = TransactionManager::New( globaldb_, io_, utxo_manager_, account_, hasher_ );
         transaction_manager_->Start();
 
         // Encode the string to UTF-8 bytes
