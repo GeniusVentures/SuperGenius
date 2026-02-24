8f12eb55c51aa5e23c80ff8fc19e814c6168995f
itsafuu <sariru@gmail.com>
2026-02-23 21:13:52 -0500

    Correct root dir

--- build/CommonBuildParameters.cmake
+++ build/CommonBuildParameters.cmake
@@ -103,8 +103,8 @@ endif()
 set(OpenSSL_DIR "${_THIRDPARTY_BUILD_DIR}/openssl/build/lib/cmake/OpenSSL" CACHE PATH "Path to OpenSSL install folder")
 set(OpenSSL_USE_STATIC_LIBS ON CACHE BOOL "OpenSSL use static libs")
 set(OpenSSL_MSVC_STATIC_RT ON CACHE BOOL "OpenSSL use static RT")
-set(OpenSSL_ROOT_DIR "${OpenSSL_DIR}" CACHE PATH "Path to OpenSSL install root folder")
-set(OpenSSL_INCLUDE_DIR "${OpenSSL_DIR}/include" CACHE PATH "Path to OpenSSL include folder")
+set(OpenSSL_ROOT_DIR "{_THIRDPARTY_BUILD_DIR}/openssl/build/" CACHE PATH "Path to OpenSSL install root folder")
+set(OpenSSL_INCLUDE_DIR "{_THIRDPARTY_BUILD_DIR}/openssl/build/include" CACHE PATH "Path to OpenSSL include folder")
 
 find_package(OpenSSL REQUIRED CONFIG)
 
