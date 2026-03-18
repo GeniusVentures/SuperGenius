1b7a7f40bc0c7b47f4ea50edc639c923314c36cb
Eduardo Menges Mattje <eduardomattje@gmail.com>
2026-03-18 16:50:11 -0300

    Added mnemonic address derivation

--- src/account/GeniusNode.hpp
+++ src/account/GeniusNode.hpp
@@ -66,17 +66,17 @@ namespace sgns
                                                 bool                is_full_node = false,
                                                 bool                use_upnp     = true );
 
-        static std::shared_ptr<GeniusNode> New( const DevConfig_st               &dev_config,
-                                                const GeniusAccount::Credentials &credentials,
-                                                bool                              autodht      = true,
-                                                bool                              isprocessor  = true,
-                                                uint16_t                          base_port    = 40001,
-                                                bool                              is_full_node = false,
-                                                bool                              use_upnp     = true );
+        static std::shared_ptr<GeniusNode> NewWithMnemonics( const DevConfig_st &dev_config,
+                                                             const char         *mnemonics,
+                                                             bool                autodht      = true,
+                                                             bool                isprocessor  = true,
+                                                             uint16_t            base_port    = 40001,
+                                                             bool                is_full_node = false,
+                                                             bool                use_upnp     = true );
 
         ~GeniusNode() override;
 
-        enum class NodeState: uint8_t
+        enum class NodeState : uint8_t
         {
             CREATING = 0,
             MIGRATING_DATABASE,
@@ -91,7 +91,7 @@ namespace sgns
         /**
          * @brief      GeniusNode Error class
          */
-        enum class Error: uint8_t
+        enum class Error : uint8_t
         {
             INSUFFICIENT_FUNDS       = 1,  ///< Insufficient funds for a transaction
             DATABASE_WRITE_ERROR     = 2,  ///< Error writing data into the database
