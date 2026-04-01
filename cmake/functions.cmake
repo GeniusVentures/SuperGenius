f91cbd63a01664936e92e4a87226ce5ca3f2b498
itsafuu <sariru@gmail.com>
2026-04-01 16:12:49 -0400

    Due to changes in commonbuildparameters protobuf .h files were not installed to the correct location

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -87,7 +87,6 @@ endif()
 function(add_proto_library NAME)
     set(SOURCES "")
     set(HEADERS "")
-    set(PB_REL_PATH "")
 
     foreach(PROTO IN ITEMS ${ARGN})
         compile_proto_to_cpp(H C PB_REL_PATH ${PROTO})
@@ -110,13 +109,13 @@ function(add_proto_library NAME)
     # target_include_directories(${NAME} PUBLIC
     # ${CMAKE_BINARY_DIR}/generated/
     # )
-    foreach(H IN ITEMS ${HEADERS})
-        set_target_properties(${NAME} PROPERTIES PUBLIC_HEADER "${H}")
-    endforeach()
+    install(TARGETS ${NAME} EXPORT supergeniusTargets)
 
-    install(TARGETS ${NAME} EXPORT supergeniusTargets
-        PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${PB_REL_PATH}
-    )
+    foreach(H IN ITEMS ${HEADERS})
+        file(RELATIVE_PATH H_REL_PATH "${CMAKE_BINARY_DIR}/generated" "${H}")
+        get_filename_component(H_REL_DIR "${H_REL_PATH}" DIRECTORY)
+        install(FILES "${H}" DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${H_REL_DIR}")
+    endforeach()
 
     disable_clang_tidy(${NAME})
 
