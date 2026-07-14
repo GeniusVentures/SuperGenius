a62fd00de3080a82ffbd97d90e3b8ba9c087cef2
Henrique A Klein <henryaklein@gmail.com>
2026-07-14 12:53:57 -0300

    fix(04.2-01): use relative include path for chain_config.hpp (submodule examples/ dir not in cmake include scope)

--- test/src/bridge_e2e/bridge_rlpx_e2e_test.cpp
+++ test/src/bridge_e2e/bridge_rlpx_e2e_test.cpp
@@ -38,7 +38,7 @@
 #include "testutil/wait_condition.hpp"
 
 #include "anvil_fixture.hpp"
-#include "chain_config.hpp"
+#include "../../../evmrelay/examples/chain_config.hpp"
 
 /**
  * @brief Decodes a base64-encoded string to raw bytes.
